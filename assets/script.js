23731bc6c0bb2a71fac6f270539a7f0f273bfd9e
edwardribas <ribasedu2005@gmail.com>
2022-01-31 09:14:49 -0300

    🚀 Some releases

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // form selector
 const form = document.querySelector("form");
+const clipboardBtn = document.getElementById("clipboard");
+clipboardBtn.onclick = e => {
+    e.preventDefault();
+}
 
 // form submit event
 form.onsubmit = e => {
@@ -23,7 +27,7 @@ form.onsubmit = e => {
 
     // operation responsible
     const calculator = () => {
-        const text = document.querySelector("#result");
+        const text = document.querySelector("#result p"); // result's output
         if (isValid === true){
             // variables declaration
             num1 = results[0], num2 = results[1];
@@ -58,15 +62,20 @@ form.onsubmit = e => {
                     break;
                 case "6": // increase
                     result = num2+percentage;
-                    operation = "% de acréscimo em ";
+                    operation = "% de acréscimo sobre ";
                     break;
                 case "7": // discount
                     result = num2-percentage;
-                    operation = "% de desconto em ";
+                    operation = "% de desconto sobre ";
             }
 
             // operation output
-            text.innerHTML = `${num1}${operation}${num2} é igual a ${result}.`;
+            text.innerHTML = `${num1}${operation}${num2} é igual a ${result}.`; // result output
+
+            // result clipboard
+            clipboardBtn.onclick = e => {
+                navigator.clipboard.writeText(num1 + operation + num2 + " é igual a " + result)
+            }
         }else{
             text.innerHTML = "Preencha todos os campos.";
         }
